9ce12bf3d5768530fd0bf24187e3cb70dc2ff256
Karen1031 <Karen.g.kg@gmail.com>
2022-07-13 09:12:18 -0700

    updated js temperture and city

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 function displayTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round (response.data.main.temp)
+  let cityElement = document.querySelector("#city");
+   temperatureElement.innerHTML = Math.round (response.data.main.temp)
+   cityElement.innerHTML = response.data.name;
 }
 let apiKey = "6f543def4e92c07bc6958213d1ba887b";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
